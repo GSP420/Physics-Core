b86b6806d381e5d2df4cb6ca6c8a610403088d54
kirabaen <kirabaen@gmail.com>
2013-09-29 14:16:31 -0400

    CCD update

--- GSP420_Physics_Core/GSP420_Physics_Core/PhysicsCollision.cpp
+++ GSP420_Physics_Core/GSP420_Physics_Core/PhysicsCollision.cpp
@@ -50,12 +50,60 @@ bool PhysicsCollision::ContinuousCollisionDetection(D3DXVECTOR3 Obj1_centerPoint
 	D3DXVECTOR3 currentDistance = ObjectDistance(Obj1_centerPoint_current, Obj2_centerPoint_current);
 
 
+	//Calculate min and max points of object 1 for previous and current positions
+	D3DXVECTOR3 Obj1_minPoint_previous = Obj1_centerPoint_previous - Obj1_extent;
+	D3DXVECTOR3 Obj1_maxPoint_previous = Obj1_centerPoint_previous + Obj1_extent;
+
+	D3DXVECTOR3 Obj1_minPoint_current = Obj1_centerPoint_current - Obj1_extent;
+	D3DXVECTOR3 Obj1_maxPoint_current = Obj1_centerPoint_current + Obj1_extent;
+
+	//Calculate the change in min and max positions between previous and current time steps
+	D3DXVECTOR Obj1_minPoint_movement = Obj1_minPoint_current - Obj1_minPoint_previous;
+	D3DXVECTOR Obj1_maxPoint_movement = Obj1_maxPoint_current - Obj1_maxPoint_previous;
+
+
+	//Calculate min and max points of object 2 for previous and current positions
+	D3DXVECTOR3 Obj2_minPoint_previous = Obj2_centerPoint_previous - Obj2_extent;
+	D3DXVECTOR3 Obj2_maxPoint_previous = Obj2_centerPoint_previous + Obj2_extent;
+
+	D3DXVECTOR3 Obj2_minPoint_current = Obj2_centerPoint_current - Obj2_extent;
+	D3DXVECTOR3 Obj2_maxPoint_current = Obj2_centerPoint_current + Obj2_extent;
+
+	//Calculate the change in min and max point positions between previous and current time steps
+	D3DXVECTOR3 Obj2_minPoint_movement = Obj2_minPoint_current - Obj2_minPoint_previous;
+	D3DXVECTOR3 Obj2_maxPoint_movement = Obj2_maxPoint_current - Obj2_maxPoint_previous;
+
+
+	//Calculate the change in distance between objects along each axis
+	D3DXVECTOR3 delta_x_distance = currentDistance.x - previousDistance.x;
+	D3DXVECTOR3 delta_y_distance = currentDistance.y - previousDistance.y;
+	D3DXVECTOR3 delta_z_distance = currentDistance.z - previousDistance.z;
+
 	if(currentDistance.x < previousDistance.x || currentDistance.y < previousDistance.y || currentDistance.z < previousDistance.z)
 	{
 		//The two objects have moved closer together
 
 		D3DXVECTOR3 Obj1_movement = ObjectDistance(Obj1_centerPoint_previous, Obj1_centerPoint_current);	//How far object 1 moved during the last time interval
 		D3DXVECTOR3 Obj2_movement = ObjectDistance(Obj2_centerPoint_previous, Obj2_centerPoint_current);	//How far object 2 moved during the last time interval
+
+		if(abs(delta_x_distance) >= abs(delta_y_distance) && abs(delta_x_distance) >= abs(delta_z_distance))
+		{
+			//Greater change along x axis
+			//Left & right faces are the fastest approaching faces
+		}
+		else
+		{
+			if(abs(delta_y_distance) >= abs(delta_x_distance) && abs(delta_y_distance) >= abs(delta_z_distance))
+			{
+				//Greater change along y axis
+				//Top & bottom faces are the fastest approaching faces
+			}
+			else
+			{
+				//Greater change along z axis
+				//Front & back faces are the fastest approaching faces
+			}
+		}
 	}
 	else
 	{
