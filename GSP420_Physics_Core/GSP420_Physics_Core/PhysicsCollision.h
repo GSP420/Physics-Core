56429e69c37f66dd1630efae443877b8f4dfb7e5
kirabaen <kirabaen@gmail.com>
2013-09-30 00:08:47 -0400

    Fixed PhysicsCollision header

--- GSP420_Physics_Core/GSP420_Physics_Core/PhysicsCollision.h
+++ GSP420_Physics_Core/GSP420_Physics_Core/PhysicsCollision.h
@@ -16,7 +16,7 @@ public:
 							float, float);
 	bool PhysicsCollision::CCD(D3DXVECTOR2 Obj1_centerPoint_current, D3DXVECTOR2 Obj1_centerPoint_future, D3DXVECTOR2 Obj1_extent, D3DXVECTOR2 Obj1_velocity,
 													D3DXVECTOR2 Obj2_centerPoint_current, D3DXVECTOR2 Obj2_centerPoint_future, D3DXVECTOR2 Obj2_extent,
-													D3DXVECTOR2 Obj2_velocity, float deltaTime, float &timeOfImpact)
+													D3DXVECTOR2 Obj2_velocity, float deltaTime, float &timeOfImpact);
 	bool CollisionDetection(vector<AABB*> &boxes, Octree* octree, bool test_z_axis);
 };
 
