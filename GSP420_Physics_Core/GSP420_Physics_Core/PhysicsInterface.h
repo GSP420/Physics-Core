938b578aac04e5f6b2c25a1a30ba8adc19fe9e3e
jberg03 <jdebrg18@hotmail.com>
2013-10-04 23:43:20 -0400

    changed interface

--- GSP420_Physics_Core/GSP420_Physics_Core/PhysicsInterface.h
+++ GSP420_Physics_Core/GSP420_Physics_Core/PhysicsInterface.h
@@ -2,20 +2,89 @@
 
 #include "PhysicsCore.h"
 #include "PhysicsCollision.h"
+#include "Octree.h"
 
 class PhysicsInterface
 {
 public:
-	virtual bool RayCast3D(D3DXVECTOR3 startPoint, D3DXVECTOR3 directionVector, list<AABB> collidables, int maxTestLimit, RayCastContact &contactOutput) = 0;
-	virtual bool RayCast2D(D3DXVECTOR2 startPoint, D3DXVECTOR2 directionVector, list<AABB> collidables, int maxTestLimit, RayCastContact &contactOutput) = 0;
-	virtual bool detectCollision(vector<AABB*> &boxes, Octree* octree, bool test_z_axis) = 0;
-	virtual bool continuousCollision(D3DXVECTOR3 Obj1, D3DXVECTOR3 Obj2);
-	virtual void setAccel(D3DXVECTOR3 acceleration) = 0;
-	virtual void setAABB(D3DXVECTOR3 min, D3DXVECTOR3 max);
-	virtual void setAABB(D3DXVECTOR3 min, D3DXVECTOR3 max, bool useContinuousCollisionDetection);
-	virtual void setAABB(D3DXVECTOR3 min, D3DXVECTOR3 max, string ID);
-	virtual void setAABB(D3DXVECTOR3 min, D3DXVECTOR3 max, string ID, bool useContinuousCollisionDetection);
-	virtual D3DXVECTOR3 getAccel() = 0;
-	virtual void setVel(D3DXVECTOR3 velocity) = 0;
-	virtual D3DXVECTOR3 getVel() = 0;
+	vector<AABB*> boxes;
+
+	void StartUp(int SCENE_SIZE);
+	void Update(float deltaTime);
+	void Shutdown();
+
+	bool RayCast3D(D3DXVECTOR3 startPoint, D3DXVECTOR3 directionVector, list<AABB> collidables, int maxTestLimit, RayCastContact &contactOutput);
+	bool RayCast2D(D3DXVECTOR2 startPoint, D3DXVECTOR2 directionVector, list<AABB> collidables, int maxTestLimit, RayCastContact &contactOutput);
+	void setAccel(D3DXVECTOR3 acceleration);
+	void setAABB(D3DXVECTOR3 min, D3DXVECTOR3 max);
+	void setAABB(D3DXVECTOR3 min, D3DXVECTOR3 max, bool useContinuousCollisionDetection);
+	void setAABB(D3DXVECTOR3 min, D3DXVECTOR3 max, string ID);
+	void setAABB(D3DXVECTOR3 min, D3DXVECTOR3 max, string ID, bool useContinuousCollisionDetection);
+	D3DXVECTOR3 getAccel();
+	void setVel(D3DXVECTOR3 velocity);
+	D3DXVECTOR3 getVel();
+
+private:
+	PhysicsCore core;
+	PhysicsCollision collide;
+	Octree* octree;
+	float timeUntilUpdate;
 };
+
+void PhysicsInterface::StartUp(int SCENE_SIZE)
+{
+	core.velocity = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	core.acceleration = D3DXVECTOR3(0.0f, float(core.GRAVITY), 0.0f);
+	timeUntilUpdate = 0.0f;
+
+	octree = new Octree(D3DXVECTOR3(-SCENE_SIZE / 2, -SCENE_SIZE / 2, -SCENE_SIZE / 2),
+						D3DXVECTOR3(SCENE_SIZE / 2, SCENE_SIZE / 2, SCENE_SIZE / 2),
+						0.0f);
+}
+
+void PhysicsInterface::Update(float dt)
+{
+	core.Accelerate(dt);
+	octree->advance(boxes, octree, dt, timeUntilUpdate); 
+}
+
+void PhysicsInterface::Shutdown()
+{
+	for(unsigned int i = 0; i < boxes.size(); i++)
+		delete boxes[i];
+
+	 delete octree;
+}
+
+bool PhysicsInterface::RayCast3D(D3DXVECTOR3 startPoint, D3DXVECTOR3 directionVector, list<AABB> collidables, int maxTestLimit, RayCastContact &contactOutput)
+{
+	return core.RayCast(startPoint, directionVector, collidables, maxTestLimit, contactOutput);
+}
+
+bool PhysicsInterface::RayCast2D(D3DXVECTOR2 startPoint, D3DXVECTOR2 directionVector, list<AABB> collidables, int maxTestLimit, RayCastContact &contactOutput)
+{
+	return core.RayCast(startPoint, directionVector, collidables, maxTestLimit, contactOutput);
+}
+
+void PhysicsInterface::setAccel(D3DXVECTOR3 acceleration)
+{
+	core.SetAcceleration(acceleration);
+}
+
+
+D3DXVECTOR3 PhysicsInterface::getAccel()
+{
+	return core.GetAcceleration();
+}
+
+
+void PhysicsInterface::setVel(D3DXVECTOR3 velocity)
+{
+	core.SetVelocity(velocity);
+}
+
+
+D3DXVECTOR3 PhysicsInterface::getVel()
+{
+	return core.velocity;
+}
